f63540168d593d8777367d4ee7ddc39d91107dbe
palomitadkaramelo <palomitadkaramelo@gmail.com>
2026-05-14 13:58:37 -0600

    Update Problemasimple4.c

--- Problemasimple4.c
+++ Problemasimple4.c
@@ -1,4 +1,17 @@
 #include <stdio.h>
 int main()
 {
-  int dinero, 
+  float inversion, tasainteres, montototal;
+
+  printf("===== BANCO ======\n");
+  printf("Ingrese la cantidad de inversion: \n");
+  scanf("%f", &inversion);
+  printf("Ingrese la tasa de interes del banco % : \n");
+  scanf("%f", &tasainteres);
+
+  montototal=((inversion*tasainteres)/100)+ inversion;
+
+  printf("La ganancia total al final del mes es de: %.2f \n", montototal);
+
+  return 0;
+}
