4bd0761c576eca454b2146acd0eef82e9252e136
palomitadkaramelo <palomitadkaramelo@gmail.com>
2026-05-15 12:12:37 -0600

    Update Problemasimple1.c

--- Problemasimple1.c
+++ Problemasimple1.c
@@ -3,17 +3,18 @@ int main()
 {
   int A, B;
   int sum, rest, multi;
+  printf("====CALCULADORA====\n");
 
-  printf("Escribe el primer numero ");
+  printf("Escribe el primer numero: \n");
   scanf("%d", &A);
-  printf("Escribe el segundo numero ");
+  printf("Escribe el segundo numero: \n");
   scanf("%d", &B);
 
   sum= A + B;
   rest= A - B;
   multi= A * B;
 
-  printf("la suma es: %d \n", suma);
+  printf("la suma es: %d \n", sum);
   printf("la resta es: %d \n", rest);
   printf("la multi es: %d \n", multi);
 
