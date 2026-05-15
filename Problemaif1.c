166582f8deb807fefeb8c6b6f4a62975d6e70dbb
palomitadkaramelo <palomitadkaramelo@gmail.com>
2026-05-15 12:31:31 -0600

    Update Problemaif1.c

--- Problemaif1.c
+++ Problemaif1.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 int main ()
 {
-int Monto, monto_final=0;
-  printf("====ALGORITMO PAGO CON DESCUENTO====\n");
-  printf("Ingrese el precio del producto que desea adquirir\n");
-  scanf("%d",&Monto);
-  if(Monto>2500)
+int monto, monto_final=0;
+  printf("====PAGO CON DESCUENTO====\n");
+  printf("Ingrese el precio del producto que desea adquirir: \n");
+  scanf("%d",&monto);
+  if(monto>2500)
   {
-    monto_final=Monto-(Monto*0.08);
-  } else 
+    monto_final=monto-(monto*0.08);
+    printf("Su producto cuenta con descuento: \n");
+    printf("El monto final con descuento aplicado es: %d\n$",monto_final);
+  }
+  else 
   {
     printf("No aplica descuento para montos menores a 2500$\n");
+    printf("El monto final es: %d \n", monto);
   }
-  
-printf("El monto final con descuento aplicado es %d\n$",monto_final);
 
 return 0;
 }
